e8f47269506f80343a8e4593a9074431a24284c3
Ben Lovy <bendlovy@gmail.com>
2017-05-05 19:50:23 -0400

    Thru ch 5

--- little-schemer/general.rkt
+++ little-schemer/general.rkt
@@ -219,3 +219,97 @@
     (cond
       ((one? n) (cdr lat))
       (else (cons (car lat) (rempickalt (sub1 n) (cdr lat)))))))
+
+(define rember*
+  (λ (a l)
+    (cond
+      ((null? l) '())
+      ((atom? (car l))
+              (cond
+                ((eq? (car l) a) (rember* a (cdr l)))
+                (else (cons (car l) (rember* a (cdr l))))))
+      (else (cons (rember* a (car l)) (rember* a (cdr l)))))))
+
+(define insertR*
+  (λ (new old l)
+    (cond
+      ((null? l) '())
+      ((atom? (car l))
+       (cond
+         ((eq? (car l) old) (cons (car l) (cons new (insertR* new old (cdr l)))))
+         (else (cons (car l) (insertR* new old (cdr l))))))
+      (else (cons (insertR* new old (car l)) (insertR* new old (cdr l)))))))
+
+(define occur*
+  (λ (a l)
+    (cond
+      ((null? l) 0)
+      ((atom? (car l))
+       (cond
+         ((eq? (car l) a) (add1 (occur* a (cdr l))))
+         (else (occur* a (cdr l)))))
+      (else (o+ (occur* a (car l)) (occur* a (cdr l)))))))
+
+(define subst*
+  (λ (new old l)
+    (cond
+      ((null? l) '())
+      ((atom? (car l))
+       (cond
+         ((eq? (car l) old) (cons new (subst* new old (cdr l))))
+         (else (cons (car l) (subst* new old (cdr l))))))
+      (else (cons (subst* new old (car l)) (subst* new old (cdr l)))))))
+
+(define insertL*
+  (λ (new old l)
+    (cond
+      ((null? l) '())
+      ((atom? (car l))
+       (cond
+         ((eq? (car l) old) (cons new (cons (car l) (insertL* new old (cdr l)))))
+         (else (cons (car l) (insertL* new old (cdr l))))))
+      (else (cons (insertL* new old (car l)) (insertL* new old (cdr l)))))))
+
+(define member*
+  (λ (a l)
+    (cond
+      ((null? l) #f)
+      ((atom? (car l)) (or (eq? (car l) a) (member* a (cdr l))))
+      (else (or (member* a (car l)) (member* a (cdr l)))))))
+
+(define leftmost
+  (λ (l)
+    (cond
+      ((atom? (car l)) (car l))
+      (else (leftmost (car l))))))
+
+(define eqlist?
+  (λ (l1 l2)
+    (cond
+      ((and (null? l1) (null? l2)) #t)
+      ((or (null? l1) (null? l2) #f))
+      ((and (atom? (car l1)) (atom? (car l2)))
+       (and (eqan? (car l1) (car l2)) (eqlist? (cdr l1) (cdr l2)))) ;here's the meat
+      ((or (atom? (car l1)) (atom? (car l2))) #f)
+      (else (and (eqlist? (car l1) (car l2)) (eqlist? (cdr l1) (cdr l2)))))))
+
+(define oequal?
+  (λ (s1 s2)
+    (cond
+      ((and (atom? s1) (atom? s2)) (eqan? s1 s2))
+      ((or (atom? s1) (atom? s2)) #f)
+      (else (eqlistalt? s1 s2)))))
+
+(define eqlistalt?
+  (λ (l1 l2)
+    (cond
+      ((and (null? l1) (null? l2)) #t)
+      ((or (null? l1) (null? l2)) #f)
+      (else (and (oequal? (car l1) (car l2)) (eqlistalt? (cdr l1) (cdr l2)))))))
+
+(define newrember
+  (λ (s l)
+    (cond
+      ((null? l) '())
+      ((oequal? (car l) s) (cdr l))
+      (else (cons (car l) (rember s (cdr l)))))))
